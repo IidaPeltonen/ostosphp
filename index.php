4263013ae7c844dc7ca24e99c8d42130f4dc44d7
IidaPeltonen <n0peii00@students.oamk.fi>
2021-10-19 13:41:32 +0300

    haku-osio toimii

--- index.php
+++ index.php
@@ -1,13 +1,9 @@
 <?php
-    /* require_once 'inc/functions.php';
-    require_once 'inc/headers.php'; */
-    header('Access-Control-Allow-Origin: *');
-    header('Content-Type: application/json');
+    require_once 'inc/functions.php';
+    require_once 'inc/headers.php'; 
 
-    /* $db = openDB(); */
-    $db = new PDO('mysql:host=localhost;dbname=shoppinglist;charset=utf8','root','');
-    $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-    
+
+    $db = openDB(); 
     $sql = "select * from item";
     $query = $db->query($sql);
     $results = $query->fetchAll(PDO::FETCH_ASSOC);
